fec7eb2058923ef5106f6521b5af73f3fbe20c95
kaustuv deolal <vutsuak96@gmail.com>
2016-11-23 03:51:04 +0530

    lat long GET POST created

--- track_server.py
+++ track_server.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+import sqlite3 as sql
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+	return 'Hello World'
+
+@app.route('/api/v1.0/track_me/position/<lat>/<longi>/<timestamp>',methods=['GET','POST'])
+def pos(lat,longi,timestamp):
+	if request.method=='GET':
+		conn = sql.connect('C:\\Users\\windows 7\\Desktop\\track_me.db')
+		cur=conn.cursor();
+		x=cur.execute("SELECT * FROM position ORDER BY id DESC LIMIT 1")
+		for i in x:
+			x=i
+		conn.commit()
+		return jsonify({'lat': x[1],'longi':x[2],'timestamp':x[3]})
+	if request.method=='POST':
+		conn = sql.connect('C:\\Users\\windows 7\\Desktop\\track_me.db')
+		cur = conn.cursor()
+		cur.execute("INSERT INTO POSITION (LAT,LONGI,TIMESTAMP)  VALUES (?,?,?)",(lat,longi,timestamp))
+		conn.commit()
+		return "Added to database"
+		
+
+	   
+	    
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
